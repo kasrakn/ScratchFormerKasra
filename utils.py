6e9c37774c0f6886d5e11c32627b26d8da5a6c60
Korminejad <KORMINES@email.sc.edu>
2025-02-21 11:14:54 -0500

    feat: add new dataset name CROPLAND indicating our own database

--- utils.py
+++ utils.py
@@ -17,6 +17,10 @@ def get_loader(data_name, img_size=256, batch_size=8, split='test',
         data_set = CDDataset(root_dir=root_dir, split=split,
                                  img_size=img_size, is_train=is_train,
                                  label_transform=label_transform)
+    elif dataset == 'CROPLAND ':
+        data_set = CDDataset(root_dir=root_dir, split=split,
+                                 img_size=img_size, is_train=is_train,
+                                 label_transform=label_transform)
     else:
         raise NotImplementedError(
             'Wrong dataset name %s (choose one from [CDDataset])'
